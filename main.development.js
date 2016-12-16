06eebec77e2de2e90aae2c91d0b3bf5e01db5f21
Architektor <gloosx@gmail.com>
2016-12-16 13:04:47 +0300

    menu

--- main.development.js
+++ main.development.js
@@ -81,6 +81,7 @@ function initializeApp() {
   }
 }
 
+
 ipcMain.on('screenshot-reject', () => {
   mainWindow.webContents.send('screenshot-reject');
 });
@@ -96,6 +97,128 @@ app.on('ready', () => {
   tray.setToolTip('Open chronos tracker');
   tray.on('click', () => mainWindow.show());
   initializeApp();
+  if (process.platform === 'darwin') {
+    template = [{
+      label: 'Chronos',
+      submenu: [{
+        label: 'About Chronos',
+        selector: 'orderFrontStandardAboutPanel:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Hide DBGlass',
+        accelerator: 'Command+H',
+        selector: 'hide:'
+      }, {
+        label: 'Hide Others',
+        accelerator: 'Command+Shift+H',
+        selector: 'hideOtherApplications:'
+      }, {
+        label: 'Show All',
+        selector: 'unhideAllApplications:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        }
+      }]
+    }, {
+      label: 'Edit',
+      submenu: [{
+        label: 'Undo',
+        accelerator: 'Command+Z',
+        selector: 'undo:'
+      }, {
+        label: 'Redo',
+        accelerator: 'Shift+Command+Z',
+        selector: 'redo:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Cut',
+        accelerator: 'Command+X',
+        selector: 'cut:'
+      }, {
+        label: 'Copy',
+        accelerator: 'Command+C',
+        selector: 'copy:'
+      }, {
+        label: 'Paste',
+        accelerator: 'Command+V',
+        selector: 'paste:'
+      }, {
+        label: 'Select All',
+        accelerator: 'Command+A',
+        selector: 'selectAll:'
+      }]
+    }, {
+      label: 'View',
+      submenu: (process.env.NODE_ENV === 'development') ? [{
+        label: 'Reload',
+        accelerator: 'Command+R',
+        click() {
+          mainWindow.webContents.send('reload');
+        }
+      }, {
+        label: 'Toggle Full Screen',
+        accelerator: 'Ctrl+Command+F',
+        click() {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        }
+      }, {
+        label: 'Toggle Developer Tools',
+        accelerator: 'Alt+Command+I',
+        click() {
+          mainWindow.toggleDevTools();
+        }
+      }] : [{
+        label: 'Toggle Full Screen',
+        accelerator: 'Ctrl+Command+F',
+        click() {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        }
+      }]
+    }, {
+      label: 'Window',
+      submenu: [{
+        label: 'Minimize',
+        accelerator: 'Command+M',
+        selector: 'performMiniaturize:'
+      }, {
+        label: 'Close',
+        accelerator: 'Command+W',
+        selector: 'performClose:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Bring All to Front',
+        selector: 'arrangeInFront:'
+      }]
+    }, {
+      label: 'Help',
+      submenu: [{
+        label: 'Learn More',
+        click() {
+          shell.openExternal('http://dbglass.web-pal.com');
+        }
+      }, {
+        label: 'Hire us!',
+        click() {
+          shell.openExternal('http://web-pal.com');
+        }
+      }, {
+        label: 'Search Issues',
+        click() {
+          shell.openExternal('https://github.com/web-pal/DBGlass/issues');
+        }
+      }]
+    }];
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  }
 });
 
 app.on('activate', () => {
