2fef4a21a247b8495546006143ac3d4a86f5d315
ignatif <ignatif@gmail.com>
2017-10-13 19:18:28 +0300

    sidebar placeholder

---
+++ app/components/Placeholders/RecentItemsPlaceholder/styled/index.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TimestampPlaceholderContainer = styled.div`
+  height: 24px;
+  padding: 0px 13px;
+  background: #fff;
+  text-align: right;
+  color: rgba(0, 0, 0, 0.7);
+  padding-bottom: 8px;
+  border-bottom: 1px solid rgba(151, 151, 151, 0.35);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+`;
+export const TimestampPlaceholder = styled.div`
+  background: #F4F5F7;
+  border-radius: 15px;
+  width: 94px;
+  height: 24px;
+`;
