0e132b97d9f402747d3635be81eadb4f363ac16a
ignatif <ignatif@gmail.com>
2017-10-13 17:06:54 +0300

    worklog add dialog

--- app/containers/Modals/WorklogModal/WorklogModal.jsx
+++ app/containers/Modals/WorklogModal/WorklogModal.jsx
@@ -10,7 +10,7 @@ import InlineEditor from '@atlaskit/inline-edit';
 import CalendarIcon from '@atlaskit/icon/glyph/calendar';
 import EditorCloseIcon from '@atlaskit/icon/glyph/editor/close';
 import Calendar from '@atlaskit/calendar';
-import  Tooltip from '@atlaskit/tooltip';
+import Tooltip from '@atlaskit/tooltip';
 import { H700 } from 'styles/typography';
 import { ModalContentContainer } from 'styles/modals';
 import { connect } from 'react-redux';
@@ -20,6 +20,8 @@ import { uiActions } from 'actions';
 import { getWorklogModalOpen } from 'selectors';
 
 import {
+  InputLabel,
+  CalendarContainer,
   CalendarIconContainer,
 } from './styled';
 
@@ -48,14 +50,70 @@ const worklogTypes = [
   },
 ];
 
+const timeOptions = [
+  {
+    items: [
+      { content: '00:00', value: '00:00' },
+      { content: '00:30', value: '00:30' },
+      { content: '01:00', value: '01:00' },
+      { content: '01:30', value: '01:30' },
+      { content: '02:00', value: '02:00' },
+      { content: '02:30', value: '02:30' },
+      { content: '03:00', value: '03:00' },
+      { content: '03:30', value: '03:30' },
+      { content: '04:00', value: '04:00' },
+      { content: '04:30', value: '04:30' },
+      { content: '05:00', value: '05:00' },
+      { content: '05:30', value: '05:30' },
+      { content: '06:00', value: '06:00' },
+      { content: '06:30', value: '06:30' },
+      { content: '07:00', value: '07:00' },
+      { content: '07:30', value: '07:30' },
+      { content: '08:00', value: '08:00' },
+      { content: '08:30', value: '08:30' },
+      { content: '09:00', value: '09:00' },
+      { content: '09:30', value: '09:30' },
+      { content: '10:00', value: '10:00' },
+      { content: '10:30', value: '10:30' },
+      { content: '11:00', value: '11:00' },
+      { content: '11:30', value: '11:30' },
+      { content: '12:00', value: '12:00' },
+      { content: '12:30', value: '12:30' },
+      { content: '13:00', value: '13:00' },
+      { content: '13:30', value: '13:30' },
+      { content: '14:00', value: '14:00' },
+      { content: '14:30', value: '14:30' },
+      { content: '15:00', value: '15:00' },
+      { content: '15:30', value: '15:30' },
+      { content: '16:00', value: '16:00' },
+      { content: '16:30', value: '16:30' },
+      { content: '17:00', value: '17:00' },
+      { content: '17:30', value: '17:30' },
+      { content: '18:00', value: '18:00' },
+      { content: '18:30', value: '18:30' },
+      { content: '19:00', value: '19:00' },
+      { content: '19:30', value: '19:30' },
+      { content: '20:00', value: '20:00' },
+      { content: '20:30', value: '20:30' },
+      { content: '21:00', value: '21:00' },
+      { content: '21:30', value: '21:30' },
+      { content: '22:00', value: '22:00' },
+      { content: '22:30', value: '22:30' },
+      { content: '23:00', value: '23:00' },
+      { content: '23:30', value: '23:30' },
+    ],
+  },
+];
+
 class WorklogModal extends Component<Props> {
   state = {
     calendarOpened: false,
+    date: undefined,
   }
 
   render() {
     const { isOpen, setWorklogModalOpen } = this.props;
-    const { calendarOpened } = this.state;
+    const { calendarOpened, date } = this.state;
 
     return (
       <ModalDialog
@@ -75,8 +133,66 @@ class WorklogModal extends Component<Props> {
           </Flex>
         }
       >
-        <ModalContentContainer style={{ height: 'auto' }}>
+        <ModalContentContainer style={{ minHeight: 360 }}>
           <H700 style={{ display: 'block' }}>Add worklog</H700>
+          <Flex column>
+            <InputLabel>Date</InputLabel>
+            <Tooltip
+              position="right"
+              description={calendarOpened ? 'Close calendar' : 'Open calendar'}
+            >
+              <Flex
+                row
+                alignCenter
+                style={{ cursor: 'pointer' }}
+                onClick={() => this.setState({ calendarOpened: !calendarOpened })}
+              >
+                <TextField
+                  placeholder="09/10/2017"
+                  value={date || ''}
+                  isLabelHidden
+                  isReadOnly
+                />
+                <CalendarIconContainer>
+                  {calendarOpened ?
+                    <EditorCloseIcon
+                      label="Close Calendar"
+                      size="medium"
+                      primaryColor="#263958"
+                    /> :
+                    <CalendarIcon
+                      label="Open Calendar"
+                      size="medium"
+                      primaryColor="#263958"
+                    />
+                  }
+                </CalendarIconContainer>
+              </Flex>
+            </Tooltip>
+          </Flex>
+          {calendarOpened &&
+            <CalendarContainer>
+              <Calendar onUpdate={date => this.setState({ date, calendarOpened: false })} />
+            </CalendarContainer>
+          }
+          <Flex row>
+            <div style={{ width: '35%', marginRight: 10 }}>
+              <SingleSelect
+                items={timeOptions}
+                label="From"
+                shouldFitContainer
+                placeholder="16:30"
+              />
+            </div>
+            <div style={{ width: '35%' }}>
+              <SingleSelect
+                items={timeOptions}
+                label="To"
+                placeholder="17:30"
+                shouldFitContainer
+              />
+            </div>
+          </Flex>
           <div style={{ width: '50%' }}>
             <SingleSelect
               items={worklogTypes}
@@ -89,59 +205,6 @@ class WorklogModal extends Component<Props> {
             shouldFitContainer
             label="Worklog comment"
           />
-          <Tooltip
-            position="right"
-            description="Open calendar"
-          >
-            <Flex
-              row
-              style={{ alignItems: 'flex-end', position: 'relative' }}
-            >
-              <TextField
-                label="Date"
-                placeholder="09/10/2017"
-              />
-              <CalendarIconContainer
-                onClick={() => this.setState({ calendarOpened: !calendarOpened })}
-              >
-                {calendarOpened ?
-                  <EditorCloseIcon
-                    label="Close Calendar"
-                    size="medium"
-                    primaryColor="#263958"
-                  /> :
-                  <CalendarIcon
-                    label="Open Calendar"
-                    size="medium"
-                    primaryColor="#263958"
-                  />
-                }
-              </CalendarIconContainer>
-            </Flex>
-          </Tooltip>
-          {calendarOpened &&
-            <Flex row style={{ marginTop: 5 }}>
-              <Calendar onUpdate={console.log} />
-            </Flex>
-          }
-          <Flex row>
-            <div style={{ width: '30%', marginRight: 10 }}>
-              <SingleSelect
-                items={worklogTypes}
-                label="From"
-                shouldFitContainer
-                placeholder="16:43"
-              />
-            </div>
-            <div style={{ width: '30%' }}>
-              <SingleSelect
-                items={worklogTypes}
-                label="To"
-                placeholder="17:43"
-                shouldFitContainer
-              />
-            </div>
-          </Flex>
         </ModalContentContainer>
       </ModalDialog>
     );
--- app/containers/Modals/WorklogModal/styled/index.jsx
+++ app/containers/Modals/WorklogModal/styled/index.jsx
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
+export const CalendarContainer = styled.div`
+  margin-top: 5px;
+  position: absolute;
+`;
+
 export const CalendarIconContainer = styled.div`
   margin-left: -30px;
-  margin-bottom: 5px;
+  margin-top: 2px;
   cursor: pointer;
+`;
+
+export const InputLabel = styled.div`
+  color: #6B778C;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.3333333333333333;
+  padding: 20px 0px 4px 0px;
 `;
