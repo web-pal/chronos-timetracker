92ab0d85a12e6781dfc9327490cdbcc4d02c63dd
Architektor <gloosx@gmail.com>
2017-10-17 20:47:42 +0300

    .

--- app/containers/Sidebar/SidebarItems/SidebarAllItems.jsx
+++ app/containers/Sidebar/SidebarItems/SidebarAllItems.jsx
@@ -60,7 +60,7 @@ const SidebarAllItems: StatelessFunctionalComponent<Props> = ({
       autoSized: true,
       // scrollToIndex: selectedIssueIndex,
       scrollToAlignment: 'center',
-      rowCount: (totalCount === 0 && fetching) ? 10 : totalCount,
+      rowCount: (totalCount === 0 && fetching) ? 10 : totalCount - 1,
       rowHeight: 101,
       // eslint-disable-next-line react/prop-types
       rowRenderer: ({ index, key, style }) => {
