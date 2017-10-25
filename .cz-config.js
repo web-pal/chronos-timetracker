e4718b6ae8c1a2299c6f7bcc97442fbe425f1eeb
Architektor <gloosx@gmail.com>
2017-10-25 13:24:21 +0300

    chore(Utilities): init cz-config

--- .cz-config.js
+++ .cz-config.js
@@ -4,7 +4,7 @@ module.exports = {
     {value: 'feat',     name: 'feat:     A new feature'},
     {value: 'fix',      name: 'fix:      A bug fix'},
     {value: 'docs',     name: 'docs:     Documentation only changes'},
-    {value: 'design'    name: 'design    Designing components nor other visual parts'},
+    {value: 'design',   name: 'design    Designing components nor other visual parts'},
     {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
     {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
     {value: 'perf',     name: 'perf:     A code change that improves performance'},
