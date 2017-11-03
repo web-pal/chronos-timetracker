95eee26aaae78dbe3956edc02fe21f320c337f93
Architektor <gloosx@gmail.com>
2017-11-03 15:37:24 +0200

    feat(Issue Detail View): virtualize worklogs
    fix various issues with a big work log data

--- app/containers/IssueView/IssueWorklogs/IssueWorklogs.jsx
+++ app/containers/IssueView/IssueWorklogs/IssueWorklogs.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import type { StatelessFunctionalComponent, Node } from 'react';
-import { Flex } from 'components';
-import { getSelectedIssue } from 'selectors';
+import { Flex, AutoSizeableList as List } from 'components';
+import { getSelectedIssue, getWorklogListScrollIndex } from 'selectors';
 import { worklogsActions } from 'actions';
 
 import WorklogItem from './WorklogItem';
@@ -13,32 +13,38 @@ import type { Issue, DeleteWorklogRequest, EditWorklogRequest } from '../../../t
 
 type Props = {
   selectedIssue: Issue,
+  scrollIndex: number,
   deleteWorklogRequest: DeleteWorklogRequest,
   editWorklogRequest: EditWorklogRequest,
 };
 
 const IssueWorklogs: StatelessFunctionalComponent<Props> = ({
   selectedIssue,
+  scrollIndex,
   deleteWorklogRequest,
   editWorklogRequest,
 }: Props): Node => (
-  <Flex column>
-    <Flex row alignCenter spaceBetween style={{ marginBottom: 25 }}>
-      <Flex row style={{ paddingBottom: 5 }}>
-        <span style={{ color: 'rgb(112,112,112)', marginRight: 5 }}>Logged today: </span>
-        <span style={{ fontWeight: 500, color: '#0052cc' }}>2h 32min</span>
-      </Flex>
-    </Flex>
-    <Flex column style={{ overflowX: 'scroll' }}>
-      {selectedIssue.fields.worklog.worklogs.map(worklog =>
-        <WorklogItem
-          key={worklog.id}
-          worklog={worklog}
-          issueKey={selectedIssue.key}
-          deleteWorklogRequest={deleteWorklogRequest}
-          editWorklogRequest={editWorklogRequest}
-        />,
-      )}
+  <Flex column style={{ flexGrow: 1 }}>
+    <Flex column style={{ flexGrow: 1 }}>
+      <List
+        listProps={{
+          rowCount: selectedIssue.fields.worklog.worklogs.length,
+          rowHeight: 120,
+          rowRenderer: ({ index, key, style }: { index: number, key: string, style: any }) => {
+            const worklog = selectedIssue.fields.worklog.worklogs[index];
+            return <WorklogItem
+              style={style}
+              key={key}
+              worklog={worklog}
+              issueKey={selectedIssue.key}
+              deleteWorklogRequest={deleteWorklogRequest}
+              editWorklogRequest={editWorklogRequest}
+            />;
+          },
+          scrollToIndex: scrollIndex,
+        }}
+        autoSized
+      />
     </Flex>
   </Flex>
 );
@@ -46,6 +52,7 @@ const IssueWorklogs: StatelessFunctionalComponent<Props> = ({
 function mapStateToProps(state) {
   return {
     selectedIssue: getSelectedIssue(state),
+    scrollIndex: getWorklogListScrollIndex(state),
   };
 }
 
