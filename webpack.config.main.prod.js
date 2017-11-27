d29ad93ca8f6f2c10af9fc5842598d5153d8c41d
Architektor <gloosx@gmail.com>
2017-11-27 17:02:14 +0200

    refactor: remove sentry link

--- webpack.config.main.prod.js
+++ webpack.config.main.prod.js
@@ -33,6 +33,7 @@ export default merge.smart(baseConfig, {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
       'process.env.DEBUG_PROD': JSON.stringify(process.env.DEBUG_PROD || 'false'),
       'process.env.SENTRY_API_KEY': JSON.stringify(process.env.SENTRY_API_KEY || ''),
+      'process.env.SENTRY_LINK': JSON.stringify(process.env.SENTRY_LINK || ''),
       'process.env.MIXPANEL_API_TOKEN': JSON.stringify(process.env.MIXPANEL_API_TOKEN || ''),
       'process.env.DISABLE_MIXPANEL': JSON.stringify(process.env.DISABLE_MIXPANEL || ''),
       'process.env.DISABLE_SENTRY': JSON.stringify(process.env.DISABLE_SENTRY || ''),
