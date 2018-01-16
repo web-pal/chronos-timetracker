a4d8bf92c124464f9a9f7c71f9bceb4782c9f73b
Ubayd <ubaidinoyatov@gmail.com>
2018-01-16 14:47:32 +0200

    fix(Browsing Issues): fix worklog scroll (#45)

--- app/styles/index.jsx
+++ app/styles/index.jsx
@@ -6,6 +6,10 @@ export const AppWrapper = styled.div`
   overflow: hidden;
 `;
 
+export const MaxHeight = styled.div`
+  height: 100%;
+`;
+
 export const FullPageSpinner = styled.div`
   display: flex;
   justify-content: center;
