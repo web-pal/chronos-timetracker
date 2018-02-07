833b49964c0e163689df69cd91157653e48db2d7
Vladimir Pal (BeDoIt) <mail.vpal@gmail.com>
2018-02-07 10:53:55 +0200

    chore: add flow header on sidebar

--- app/actions/ui.js
+++ app/actions/ui.js
@@ -9,7 +9,7 @@ import * as actionTypes from './actionTypes';
 
 export const setUiState = (
   key: string,
-  value: string | boolean | null,
+  value: any,
 ): UiAction => ({
   type: actionTypes.SET_UI_STATE,
   payload: {
