6350a6c554b5f51f666a1845921dd1be89f8fc13
Architektor <gloosx@gmail.com>
2018-03-06 17:59:00 +0200

    chore: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,16 @@
 
 releaseType=$1
 
-node ./bumpVersion.js ${releaseType} | \
-  git commit -a -m -F - && \
-  conventional-changelog -p angular -i CHANGELOG.md -s -k ./app/package.json && \
-  git commit -a -m 'chore: update CHANGELOG' && \
-  gf release publish && \
-  echo "new ${releaseType} release is ready for building!"
+version=$(node ./scripts/bumpVersion.js ${releaseType})
+
+git flow release start ${version}
+
+git commit -a -m "chore: bump version to ${version}"
+
+conventional-changelog -p angular -i CHANGELOG.md -s -k ./app/package.json
+
+git commit -a -m 'chore: update CHANGELOG'
+
+git flow release publish ${version}
+
+echo 'done!'
