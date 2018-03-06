7a434b6f96e6cec71ed1b137d42aa7034033e43b
Architektor <gloosx@gmail.com>
2018-03-06 17:59:25 +0200

    chore: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-releaseType=$1
+releaseType=${1:?USAGE: yarn release <releaseType>. ('major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease')}
 
-version=$(node ./scripts/bumpVersion.js ${releaseType})
+version=$(node ./scripts/bumpVersion.js ${releaseType} --dry)
 
 git flow release start ${version}
+
+node ./scripts/bumpVersion.js ${releaseType}
 
 git commit -a -m "chore: bump version to ${version}"
 
