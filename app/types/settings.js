c75b3208be1836ec3e8e35eac101eee43a55fffd
Ilya Lopukhin <gloosx@gmail.com>
2018-04-05 13:29:56 +0300

    syntax error

--- app/types/settings.js
+++ app/types/settings.js
@@ -15,7 +15,7 @@ export type SettingsAction =
   {|
     type: typeof actionTypes.SET_SETTINGS_MODAL_TAB,
     tabName: string,
-  |}
+  |} |
   {|
     type: typeof actionTypes.CLEAR_ELECTRON_CACHE,
   |};
