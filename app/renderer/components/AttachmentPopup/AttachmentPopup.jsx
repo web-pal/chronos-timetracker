c9d996ad0b9b854d3a161314e034c9c94422c194
Architektor <gloosx@gmail.com>
2019-02-19 19:39:02 +0200

    fix(Attachement Viever): styles broken af

--- app/renderer/components/AttachmentPopup/AttachmentPopup.jsx
+++ app/renderer/components/AttachmentPopup/AttachmentPopup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import rightArrow from 'images/rigth-arrow.png';
 import leftArrow from 'images/left-arrow.png';
-import downloadButtonIcon from 'images/download_icon.png';
+import DownloadIcon from '@atlaskit/icon/glyph/download';
 import DownloadAttachment from 'components/AttachmentPopup/DownloadAttachment';
 import {
   MainAttachmentContainer,
@@ -21,75 +21,96 @@ import {
   Arrow,
 } from './styled/index';
 
-import ShowAllAttachment from './ShowAllAttachment/ShowAllAttachment';
+import AttachmentsList from './AttachmentsList/AttachmentsList';
 
 
-const AttachmentPopupComponent = ({ ...props }) => (
-  <MainAttachmentContainer>
-    <HeaderContainer>
-      <TitleContainer>
-        <TitleText>
-          {props.attachmentItem.filename}
-        </TitleText>
-      </TitleContainer>
-      <DownloadButtonContainer>
-        <DownloadButton
-          href={props.attachmentItem.content}
-          download={props.attachmentItem.filename}
-        >
-          <DownloadButtonIcon
-            src={downloadButtonIcon}
-            alt="downloadIcon"
-          />
-        </DownloadButton>
-      </DownloadButtonContainer>
-    </HeaderContainer>
-    <MainContentContainer>
-      <LeftButtonContainer>
-        <LeftButton
-          onClick={props.prevAttachment}
-          disabled={props.activeIndex === 0}
-        >
-          <Arrow
-            src={leftArrow}
-            alt="arrow"
-          />
-        </LeftButton>
-      </LeftButtonContainer>
-      <MainImageContainer>
-        {
-          props.attachmentItem.mimeType.indexOf('application')
-            ? (
-              <AttachmentImage
-                src={props.attachmentItem.content}
-                alt="image"
+class AttachmentPopupComponent extends React.PureComponent {
+  state = {
+    zoom: 1,
+  };
+
+  render() {
+    const {
+      attachmentItem,
+      prevAttachment,
+      nextAttachment,
+      activeIndex,
+      attachmentSize,
+      selectAttachment,
+      attachment,
+    } = this.props;
+
+    const { zoom } = this.state;
+
+    return (
+      <MainAttachmentContainer>
+        <HeaderContainer>
+          <TitleContainer>
+            <TitleText>
+              {attachmentItem.filename}
+            </TitleText>
+          </TitleContainer>
+          <DownloadButtonContainer>
+            <DownloadButton
+              href={attachmentItem.content}
+              download={attachmentItem.filename}
+            >
+              <DownloadIcon />
+            </DownloadButton>
+          </DownloadButtonContainer>
+        </HeaderContainer>
+        <MainContentContainer>
+          <LeftButtonContainer>
+            <LeftButton
+              onClick={prevAttachment}
+              disabled={activeIndex === 0}
+            >
+              <Arrow
+                src={leftArrow}
+                alt="arrow"
               />
-            )
-            : (
-              <DownloadAttachment
-                attachment={props.attachmentItem}
+            </LeftButton>
+          </LeftButtonContainer>
+          <MainImageContainer>
+            {
+              /(jpeg|jpg|png|svg)/.test(attachmentItem.mimeType)
+                ? (
+                  <AttachmentImage
+                    src={attachmentItem.content}
+                    zoom={zoom}
+                    onClick={() => this.setState(({ zoom }) => ({
+                      zoom: zoom === 4 ? 1 : zoom + 1,
+                    }))}
+                    alt="image"
+                  />
+                )
+                : (
+                  <DownloadAttachment
+                    attachment={attachmentItem}
+                  />
+                )
+            }
+          </MainImageContainer>
+          <RightButtonContainer>
+            <RightButton
+              onClick={nextAttachment}
+              disabled={attachmentSize - 1 === activeIndex}
+            >
+              <Arrow
+                src={rightArrow}
+                alt="arrow"
               />
-            )
-        }
-      </MainImageContainer>
-      <RightButtonContainer>
-        <RightButton
-          onClick={props.nextAttachment}
-          disabled={props.attachmentSize - 1 === props.activeIndex}
-        >
-          <Arrow
-            src={rightArrow}
-            alt="arrow"
-          />
-        </RightButton>
-      </RightButtonContainer>
-    </MainContentContainer>
-    <ShowAllAttachment
-      selectAttachment={props.selectAttachment}
-      attachment={props.attachment}
-    />
-  </MainAttachmentContainer>
-);
+            </RightButton>
+          </RightButtonContainer>
+        </MainContentContainer>
+        <AttachmentsList
+          selectAttachment={selectAttachment}
+          attachment={attachment}
+        />
+      </MainAttachmentContainer>
+    );
+  }
+}
 
 
 export default AttachmentPopupComponent;
