ff86cc186700d55ff25dac4a5e82dc7fdaedff2f
teorge <georgij.vozhdayenko@gmail.com>
2019-04-24 15:54:46 +0300

    fix: fix styles for <TeamStatusList/>

--- app/renderer/containers/Popups/TeamStatusList/styled/index.jsx
+++ app/renderer/containers/Popups/TeamStatusList/styled/index.jsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-
-import Avatar from '@atlaskit/avatar';
 import SettingsIconAK from '@atlaskit/icon/glyph/settings';
 import EditIconAK from '@atlaskit/icon/glyph/edit';
 
@@ -16,7 +14,15 @@ export const TeamStatusListWrapper = styled.div`
 
 export const TeamMembersWrapper = styled.div`
   height: 285px;
-  overflow: auto;
+  overflow-y: auto;
+`;
+
+export const NoTeamSelectedText = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const TeamMemberItemWrapper = styled.div`
@@ -34,8 +40,17 @@ export const TeamMemberItemWrapper = styled.div`
   }
 `;
 
-export const UserAvatar = styled(Avatar)`
-  flex: 0 0 30%;
+export const UserAvatar = styled.img`
+  height: 48px;
+  width: 48px;
+  border: 2px solid white;
+  border-radius: 50%;
+  transition: all .1s ease-in;
+  user-select: none;
+  :hover {
+    box-shadow:0 1px 4px rgba(0, 0, 0, 0.4), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+    border-radius: 50%;
+  }
 `;
 
 export const UserInfoWrapper = styled.div`
@@ -47,6 +62,14 @@ export const UserInfoWrapper = styled.div`
 
 export const UserName = styled.span`
   font-weight: bold;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  max-width: 160px;
+
+  &:hover {
+    white-space: initial;
+  }
 `;
 
 export const UserLocation = styled.span`
@@ -57,8 +80,7 @@ export const UserLocation = styled.span`
 export const Status = styled.span``;
 
 export const LastDate = styled.span`
-  flex: 0 0 30%;
-  text-align: right;
+  flex: 0 0 29%;
 `;
 
 export const FooterToolbar = styled.div`
