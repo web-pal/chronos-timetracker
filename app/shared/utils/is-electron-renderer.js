41fc475bc5b3900881c6d330ec72161697468f79
Vivek <viv3kananthan@gmail.com>
2020-10-01 11:51:14 +0530

    Refactoring

--- app/shared/utils/is-electron-renderer.js
+++ app/shared/utils/is-electron-renderer.js
@@ -1,14 +1,7 @@
 function isRenderer() {
-  // running in a web browser
-  if (typeof process === 'undefined') return true;
-
-  // node-integration is disabled
-  if (!process) return true;
-
-  // We're in node.js somehow
-  if (!process.type) return false;
-
-  return process.type === 'renderer';
+  // Running in a web browser or node-integration is disabled then True else
+  // We're in node.js somehow 
+  return (typeof process === 'undefined' || !process) ? true : (!process.type ? false : process.type === 'renderer');
 }
 
 export default isRenderer();
